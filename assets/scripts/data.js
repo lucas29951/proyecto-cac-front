e2db2fd005b686f0590838f3bc85088bf665a36d
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-15 14:08:13 -0300

    Se modificaron los datos cargados

---
+++ assets/scripts/data.js
@@ -0,0 +1,75 @@
+
+const eventos = [
+    {
+        titulo: "Concierto de Rock",
+        categoria: "Musica",
+        fechaHora: "2024-04-10 18:00",
+        ubicacion: "Teatro Nacional",
+        descripcion: "Disfruta de un concierto en vivo con las mejores bandas de rock del momento.",
+        precio: 5500,
+        imagenUrl: "./assets/images/webp/stage-party.webp"
+    },
+    {
+        titulo: "Exposición de Arte Moderno",
+        categoria: "Arte",
+        fechaHora: "2024-04-15 15:00",
+        ubicacion: "Museo de Arte",
+        descripcion: "Descubre obras de arte contemporáneo de artistas locales e internacionales.",
+        precio: 3000,
+        imagenUrl: "./assets/images/webp/art-gallery.webp"
+    },
+    {
+        titulo: "Fiesta del Estudiante",
+        categoria: "Entretenimiento",
+        fechaHora: "2024-06-25 21:30",
+        ubicacion: "Plaza de la Musica",
+        descripcion: "Te invitamos a la fiesta mas grande para festejar el Dia del Estudiante.",
+        precio: 900,
+        imagenUrl: "./assets/images/webp/public-party.webp"
+    },
+    {
+        titulo: "Microfono Abierto - OpenCast",
+        categoria: "Entretenimiento",
+        fechaHora: "2025-02-20 19:40",
+        ubicacion: "Teatro Hercules",
+        descripcion: "Unete a la ronda de anecdotas y charlas distendidas.",
+        precio: 150,
+        imagenUrl: "./assets/images/webp/open-podcast.webp"
+    },
+    {
+        titulo: "Lectura del libro Cien Años de Soledad",
+        categoria: "Arte",
+        fechaHora: "2025-01-21 14:30",
+        ubicacion: "Salon Bellas Alas",
+        descripcion: "Forma parte de la historia mas vendida y mas iconica de la literatura.",
+        precio: 2000,
+        imagenUrl: "./assets/images/webp/lectors-group.webp"
+    },
+    {
+        titulo: "Festival de la Chacarera",
+        categoria: "Musica",
+        fechaHora: "2024-10-03 22:00",
+        ubicacion: "Estadio River Plate",
+        descripcion: "Se parte del festival de chacarera y renueva tu cultura.",
+        precio: 3200,
+        imagenUrl: "./assets/images/webp/concert-audience.webp"
+    },
+    {
+        titulo: "Muestra y Degustacion de Articulos Comestibles",
+        categoria: "Comida",
+        fechaHora: "2024-12-05 20:45",
+        ubicacion: "Nodo Tecnologico",
+        descripcion: "Ven y disfruta de los mejores productos del mercado y descubre nuevos sabores.",
+        precio: 6000,
+        imagenUrl: "./assets/images/webp/art-showcase.webp"
+    },
+    {
+        titulo: "Sesion de Radio Abierta al Publico",
+        categoria: "Entretenimiento",
+        fechaHora: "2024-09-11 09:00",
+        ubicacion: "Salon Azul",
+        descripcion: "Participa del programa de Radio Nacional, esta vez abierta al publico en general.",
+        precio: 0,
+        imagenUrl: "./assets/images/webp/podcast-session.webp"
+    },
+];
