07bd205741d59a206528d78c35da5e33c35024bd
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-16 12:55:01 -0300

    Se agrego un script con el manejo de eventos

---
+++ assets/scripts/events.js
@@ -0,0 +1,29 @@
+
+// Event listeners para cargar los eventos
+document.addEventListener("DOMContentLoaded", cargarEventos);
+
+
+// Event listeners para ordenar los eventos segun X criterio
+document.querySelector("#orders-select").addEventListener("change", (event) => {
+    const criterio = event.target.value;
+    ordenarEventos(criterio);
+});
+
+
+// Event listeners para filtrar los eventos por categoria
+document.querySelectorAll(".categorie-link").forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        const categoria = link.textContent.trim();
+        filtrarPorCategoria(categoria);
+    });
+});
+
+
+// Event listeners para buscar eventos
+document.querySelector("#search-input").addEventListener("keypress", (event) => {
+    if (event.key === 'Enter') {
+        const palabraClave = event.target.value.trim();
+        buscarEventos(palabraClave);
+    }
+});
