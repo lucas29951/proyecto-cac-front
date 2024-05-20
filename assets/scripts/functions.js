97d3edb3d15857339e6461cd5e9194564e4293f4
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-20 09:56:21 -0300

    Se agrego una funcion para mostrar los eventos en la pagina principal

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,4 +1,44 @@
 
+// Función para cargar eventos en la página principal
+function cargarEventos() {
+    const eventosContainer = document.querySelector(".events .row");
+    eventosContainer.innerHTML = "";
+
+    eventos.forEach(evento => {
+        const eventoHTML = `
+            <div class="col">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${evento.imgenUrl}" class="img-fluid rounded-start h-100" alt="${evento.titulo}">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${evento.titulo}</h5>
+                                <p class="card-text"><small class="text-muted">Fecha y hora: ${evento.fechaHora}</small></p>
+                                <p class="card-text"><small class="text-muted">Ubicación: ${evento.ubicacion}</small></p>
+                                <p class="card-text"><small class="text-muted">Precio: $${evento.precio}</small></p>
+                                <a href="./details.html" class="btn btn-primary comprar-btn" data-evento='${JSON.stringify(evento)}'>Comprar</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+        eventosContainer.innerHTML += eventoHTML;
+    });
+
+    document.querySelectorAll(".comprar-btn").forEach(button => {
+        button.addEventListener("click", (event) => {
+            const evento = JSON.parse(event.target.getAttribute("data-evento"));
+            console.log("Evento Seleccionado: " + evento);
+            localStorage.setItem("eventoSeleccionado", JSON.stringify(evento));
+        });
+    });
+}
+
+
+
 // Función para cargar eventos recibidos en la página principal
 function cargarLista(listaEventos) {
     const eventosContainer = document.querySelector(".events .row");
