e152f7fa20e73a101f61a05709275ba4820ef0c3
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-21 16:13:04 -0300

    Se eliminaron comentarios innecesarios

--- assets/scripts/details.js
+++ assets/scripts/details.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
     if (evento) {
         document.querySelector(".banner-event img").src = evento.imagen;
         document.querySelector(".banner-event img").alt = evento.titulo;
-        // document.querySelector(".info-event h5").textContent = `Fecha de Realización: ${new Date(evento.fechaHora).toLocaleString()}`;
         document.querySelector(".info-event .name-event").textContent = evento.titulo;
         document.querySelector(".info-event .description").textContent = evento.descripcion;
         document.querySelector(".info-event .date-time").textContent = new Date(evento.fechaHora).toLocaleString();
