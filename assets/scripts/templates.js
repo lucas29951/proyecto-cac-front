6c5a41982398df5abebbfe636ded66775a0ba69e
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-05-21 16:13:28 -0300

    Se eliminaron comentarios innecesarios

--- assets/scripts/templates.js
+++ assets/scripts/templates.js
@@ -10,8 +10,6 @@ const templates = {
                 <div class="card-body">
                     <h5 class="card-title">${evento.titulo}</h5>
                     <p class="card-text"><small class="text-muted">Fecha y hora: ${evento.fechaHora}</small></p>
-                    
-                    
                 </div>
                 <div class="card-footer">
                 <a href="./details.html" class="btn btn-primary comprar-btn" data-evento='${JSON.stringify(evento)}'>Comprar</a>
