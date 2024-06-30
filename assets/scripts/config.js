c327a4182c33394f24d74ee099ce0db1d95125b9
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-06-30 16:21:30 -0300

    Se modifico la ruta de la API del back

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,4 +1,4 @@
 const api = {
     eventos: 'assets/data/data.json',
-    eventapi: 'https://proyecto-cac-back.onrender.com/api/'
+    eventapi: 'https://proyecto-cac-back.onrender.com/api/events'
 }
