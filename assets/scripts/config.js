8eeddfa651911fdab14c703df0f0fadecc786f7d
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-01 21:34:47 -0300

    Endpoind agregado para la peticion de Categorias

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,4 +1,5 @@
 const api = {
     eventos: 'assets/data/data.json',
-    eventapi: 'https://proyecto-cac-back.onrender.com/api/events'
+    eventapi: 'https://proyecto-cac-back.onrender.com/api/events',
+    categoryapi: 'https://proyecto-cac-back.onrender.com/api/categories'
 }
