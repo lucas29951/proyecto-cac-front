825582e9fadd3603ad7741b56ff55a1cc332756e
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-04 11:32:44 -0300

    Modificacion de elemento del DOM

--- assets/scripts/auth.js
+++ assets/scripts/auth.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
         console.log("HAY UNA SESION INICIADA!");
         const navbar = document.querySelector(".navbar-groups");
-        const registerLink = navbar.querySelector('a[href="./register"]');
+        const registerLink = document.querySelector('a[href="./register"]');
         console.log(navbar);
         console.log(registerLink);
 
