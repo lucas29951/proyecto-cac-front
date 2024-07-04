9c36c5e6879b2b9130b069d6aaf463e157d36b29
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-04 12:40:01 -0300

    Estructura de la pagina modificada y script agregado

---
+++ assets/scripts/profile.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      const titulo = document.querySelector(".head-profile h2");
+      titulo.textContent = "Usuario: " + user.username;
+    }
+  });
