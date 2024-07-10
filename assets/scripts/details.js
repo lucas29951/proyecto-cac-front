2381920c348f10d5e4d6acbeeaea9faaa6f296a8
Lucas Rodriguez <lucasg29r95@gmail.com>
2024-07-10 18:39:58 -0300

    Se implemento la funcion para realizar la compra de un ticket

--- assets/scripts/details.js
+++ assets/scripts/details.js
@@ -1,6 +1,9 @@
 
+const detailsContainer = document.getElementById('details-group');
+
 document.addEventListener("DOMContentLoaded", () => {
     const evento = JSON.parse(localStorage.getItem("eventoSeleccionado"));
+
     if (evento) {
         document.querySelector(".banner-event img").src = evento.imagen;
         document.querySelector(".banner-event img").alt = evento.titulo;
@@ -9,5 +12,45 @@ document.addEventListener("DOMContentLoaded", () => {
         document.querySelector(".info-event .date-time").textContent = new Date(evento.fechaHora).toLocaleString();
         document.querySelector(".info-event .ubication").innerHTML = `${evento.ubicacion}`;
         document.querySelector(".buy-ticket .price").textContent = `Precio de Entrada $${evento.precio}`;
+    }
+});
+
+document.getElementById('btn-compra').addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const userLogged = JSON.parse(localStorage.getItem('user'));
+    const eventSelected = JSON.parse(localStorage.getItem('eventoSeleccionado'));
+
+    if (!userLogged || !eventSelected) {
+        alert("Error: No se encontro informacion del usuario o evento.");
+        return;
+    }
+
+    const newTicket = {
+        user_id: userLogged.id,
+        event_id: eventSelected.id,
+        quantity: 1,
+        total_price: eventSelected.precio
+    }
+
+    try {
+        const response = await fetch('https://proyecto-cac-back.onrender.com/api/tickets', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${userLogged.token}`
+            },
+            body: JSON.stringify(newTicket)
+        });
+
+        if(!response.ok) {
+            throw new Error('Error al crear el ticket.');
+        }
+
+        alert("Compra Realizada!");
+        window.location.href = './profile.html';
+    } catch (error) {
+        console.error('Error: ' + error);
+        alert('Hubo un problema al realizar la compra. Por favor, intentelo de nuevo.');
     }
 });
